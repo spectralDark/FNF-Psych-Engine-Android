b2b38233faf8abc352ed0804578860ab3e7d12d8
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-23 17:21:11 +0200

    Update MenuCharacterEditorState.hx

--- source/editors/MenuCharacterEditorState.hx
+++ source/editors/MenuCharacterEditorState.hx
@@ -411,7 +411,7 @@ class MenuCharacterEditorState extends MusicBeatState
 
 			#if android
                         sys.io.File.saveContent(Main.getDataPath() + characterName + ".json", data.trim());
-                        android.AndroidTools.toast("File Saved Successfully!!", 1);
+                        android.AndroidTools.toast("File Saved Successfully!!");
 			#else
 			_file = new FileReference();
 			_file.addEventListener(Event.COMPLETE, onSaveComplete);
